2b6ed104fe6d83364ca292d6f369e7690efe2ac6
Olga Morozova <moros0307@gmail.com>
2024-02-08 13:03:24 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://ded3d8cf-383c-4883-acd5-8bae5198868a.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = '/api/logs/main/'
+USERS_TABLE_PATH = '/api/db/resources/user_model.csv'
+CREATE_USER_PATH = '/api/v1/users/'
+PRODUCTS_KITS_PATH = '/api/v1/products/kits'
